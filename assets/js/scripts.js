a2e8323d00807eb4e45e7857adb710ea33700869
Nataliya Zhuravleva <natalindria@gmail.com>
2020-10-30 16:06:58 -0700

    Add result title to show before the result string.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,24 +3,23 @@
 //function that returns array with a range of numbers from 0 to the user inputted number with required exceptions.
 function beepBoop(number) {
   result = [];
-  if (number && number >= 0) {
-    for (let i = 0; i <= number; i++) {
-      result[i] = String(i);
-    };
-    for (i = 0; i < result.length; i++) {
-      if (result[i].includes("3")) {
-        result[i] = "Won't you be my neighbor?";
-      } else if (result[i].includes("2")) {
-        result[i] = "Boop!";
-      } else if (result[i].includes("1")) {
-        result[i] = "Beep!";
-      }
+
+  for (let i = 0; i <= number; i++) {
+    result[i] = String(i);
+  };
+  for (i = 0; i < result.length; i++) {
+    if (result[i].includes("3")) {
+      result[i] = "Won't you be my neighbor?";
+    } else if (result[i].includes("2")) {
+      result[i] = "Boop!";
+    } else if (result[i].includes("1")) {
+      result[i] = "Beep!";
     }
-  } else {
-    return false
   }
   return result;
 }
+
+
 
 //function that transforms array elements into a string with the array elements separated by comma and space. Period after last element.
 function showArray(array) {
@@ -39,12 +38,16 @@ function showArray(array) {
 $(document).ready(function () {
   $("#form").submit(function (event) {
     event.preventDefault();
-    
     const input = $("#input").val();
+
     if (input && input >= 0) {
+     
       $("#warning").hide();
-      let res = showArray(beepBoop(input));
-      $("#result").append(" " + res).show();
+      res = showArray(beepBoop(input));
+      $("#result-title").show();
+     $("#result").text(res).show();
+    
+     
     } else {
       $("#result").hide();
       $("#warning").show();
